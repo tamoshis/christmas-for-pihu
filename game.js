549cdb4c1668ddae3f05758f52bfe8256115425c
tamoshis <paultamasish@gmail.com>
2025-12-25 00:32:33 +0530

    Add openGift function to display messages

---
+++ game.js
@@ -0,0 +1,10 @@
+function openGift(num) {
+  let msg = "";
+
+  if (num === 1) msg = "Tui amar kache khub special ❤️";
+  if (num === 2) msg = "Tui caring ar soft heart er manus 😌";
+  if (num === 3) msg = "Ektu rag kore but cute 😜";
+  if (num === 4) msg = "Ei Christmas e tui sobsomoy happy thakis 🎄❤️";
+
+  document.getElementById("message").innerText = msg;
+}
